1e989fb346e3c7d33de2402b1959341c983af274
Ketan <kpatel7343@conestoga.on.ca>
2023-07-13 19:32:23 -0400

    files added

--- Assignment_3/Assignment3.py
+++ Assignment_3/Assignment3.py
@@ -1,7 +1,3 @@
-# Importing required libraries
-
-# pip install selenium
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -9,55 +5,56 @@ import time
 # Setting up the webdriver
 driver = webdriver.Chrome()
 
-# Navigating to the Amazon.ca homepage
-driver.get("https://www.amazon.ca")
+# Navigating to the LinkedIn login page
+driver.get("https://www.linkedin.com/login")
 time.sleep(3)
 
-# Finding the search bar and entering text
-# search_bar = driver.find_element_by_id("id","twotabsearchtextbox") old syntax
-search_bar = driver.find_element("id","twotabsearchtextbox")
-search_bar.send_keys("mobile phone")
+# Finding the email and password input fields
+email_field = driver.find_element("id", "username")
+password_field = driver.find_element("id", "password")
 
-# Submitting the search query
+# Entering the email and password
+email_field.send_keys("ketan95744@gmail.com")
+password_field.send_keys("Ketan@6800")
+
+# Submitting the login form
+password_field.send_keys(Keys.RETURN)
+
+# Waiting for the home page to load
+time.sleep(5)
+
+# Verifying that the login was successful
+assert "LinkedIn" in driver.title
+
+# Searching for a connection
+search_bar = driver.find_element("class name", "search-global-typeahead__input")
+search_bar.send_keys("John Doe")
 search_bar.send_keys(Keys.RETURN)
 
 # Waiting for the search results page to load
 time.sleep(5)
 
-# Verifying that the search results page has loaded
-assert "mobile phone" in driver.title
+# Clicking on a search result
+search_result = driver.find_element("class name", "search-result__result-link")
+search_result.click()
 
-# Selecting a laptop from the search results
-laptop_link = driver.find_element("xpath","/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[9]/div/div/div/div/div[1]/span/a/div/img")
-# laptop_link = driver.find_element("By.CSS_SELECTOR","span[data-component-type='s-product-image'] a")
-laptop_link.click()
-
-
-
-# Waiting for the laptop details page to load
+# Waiting for the profile page to load
 time.sleep(5)
 
-# Adding the laptop to the cart
-add_to_cart_button = driver.find_element("id","add-to-cart-button")
-add_to_cart_button.click()
+# Sending a message to the connection
+message_button = driver.find_element("class name", "message-anywhere-button")
+message_button.click()
 
-# Waiting for the cart to update
+# Typing the message
+message_input = driver.find_element("class name", "msg-form__contenteditable")
+message_input.send_keys("Hello, John! How are you?")
+
+# Sending the message
+send_button = driver.find_element("class name", "msg-form__send-button")
+send_button.click()
+
+# Waiting for the message to be sent
 time.sleep(5)
-
-# Clicking on no thanks button
-   # no_thanks_button= driver.find_element("xpath","/html/body/div[9]/div[3]/div[1]/div/div/div[2]/div[2]/div/div/div[3]/div/span[2]/span/input")
-    #no_thanks_button.click()
-    #time.sleep(2)
-
-proceed_to_checkout= driver.find_element("xpath","/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[3]/div/div[1]/form/span/span/span/input")
-proceed_to_checkout.click()
-time.sleep(2)
-
-
-# Verifying that the laptop has been added to the cart
-# cart_count = driver.find_element("id","nav-cart-count")
-# assert cart_count.text == "1"
-# cart_count.click()
 
 # Closing the webdriver
 driver.close()
